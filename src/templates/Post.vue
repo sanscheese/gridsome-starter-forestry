0c1bbcef1b73a77ab2e0ca9ca9640791d7ea0209
Ben Sheedy <ben.sheedy@digitalrisks.co.uk>
2019-04-02 14:29:26 +0100

    Updating post template

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -8,7 +8,12 @@ export default {
   <Layout>
     <h1>{{ $page.post.title }}</h1>
 
-    <p>{{ $page.post.content }}</p>
+    <div v-for="component in $page.post.components">
+      <h5>{{component.template}}</h5>
+      
+      <h2>{{component.title}}</h2>
+
+    </div>
   </Layout>
 </template>
 
@@ -18,6 +23,10 @@ query Post($path: String!) {
     id
     title
     path
+    components {
+      template
+      title
+    }
   }
 }
 </page-query>
