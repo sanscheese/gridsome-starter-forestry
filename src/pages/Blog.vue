bff8dbfc5bc38910e21a6b118a2cd1377af4510a
Ben Sheedy <ben.sheedy@digitalrisks.co.uk>
2019-04-02 11:44:48 +0100

    basic blog page

---
+++ src/pages/Blog.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <h1>Blog</h1>
+    <div v-for="{ node } in $static.allPost.edges">
+      <h3><g-link :to="node.path">{{node.title}}</g-link></h3>
+    </div>
+  </Layout>
+</template>
+
+<script>
+// export default {
+//   metaInfo: {
+//     title: 'About us'
+//   }
+// }
+</script>
+
+<static-query>
+query {
+  allPost {
+    edges {
+      node {
+        title
+        path
+        slug
+        content
+        fileInfo {
+          name,
+          path,
+          directory
+        }
+      }
+    }
+  }
+}
+</static-query>
